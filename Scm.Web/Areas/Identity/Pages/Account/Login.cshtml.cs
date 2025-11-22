2e7257a8b9dc79e4ed4907f0a295e73c70d7a170
Deadlord <ali.gasanow9@gmail.com>
2025-11-22 17:17:45 +0200

    .fix tickts

--- Scm.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Scm.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Scm.Infrastructure.Identity;
+using Scm.Domain.Identity;
 
 namespace Scm.Web.Areas.Identity.Pages.Account;
 
